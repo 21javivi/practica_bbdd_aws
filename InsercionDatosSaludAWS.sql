d89db5f77d05d2c579cd673643d67066d0e15e74
Javier Galán Cortés <javier.galan2032@gmail.com>
2024-05-15 12:27:09 +0200

    Versión 1 InserciónDatosSaludAWS

---
+++ InsercionDatosSaludAWS.sql
@@ -0,0 +1,9 @@
+SET SQL_SAFE_UPDATES = 0;
+INSERT INTO usuario VALUES('00000000A','Javier','Galán Cortés','2002-09-21','H');
+INSERT INTO usuario VALUES('11111111B','Enrique','Lopez Sanchez','1996-05-15','H');
+INSERT INTO usuario VALUES('22222222C','Belén','Galán Cortés','2007-02-28','M');
+
+SELECT * FROM usuario;
+
+
+SET SQL_SAFE_UPDATES = 1;
