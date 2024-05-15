cff612703999181a739b9f39f5d110588456debe
Javier Galán Cortés <javier.galan2032@gmail.com>
2024-05-15 10:53:51 +0200

    Versión 1 BaseDeDatosSaludAWS

---
+++ BaseDeDatosSaludAWS.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE IF NOT EXISTS salud;
+
+USE salud;
+
+CREATE TABLE IF NOT EXISTS usuario(
+dni VARCHAR(9) NOT NULL,
+nombre VARCHAR(20) NOT NULL,
+apellidos VARCHAR(50) NOT NULL,
+fechaNacimiento DATE,
+PRIMARY KEY(dni)
+);
+
+CREATE TABLE IF NOT EXISTS comida(
+dni VARCHAR(9) NOT NULL,
+plato VARCHAR(15) NOT NULL,
+fechaComida DATE NOT NULL,
+tipo VARCHAR(15) NOT NULL,
+calorias INT(4),
+FOREIGN KEY (dni) REFERENCES usuario(dni)
+);
+
+CREATE TABLE IF NOT EXISTS total(
+dni VARCHAR(9) NOT NULL,
+fechaComida DATE NOT NULL,
+caloriasTotales INT(5),
+FOREIGN KEY (dni) REFERENCES usuario(dni)
+);
